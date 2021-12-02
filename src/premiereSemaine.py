c12077845cc40b6e02df17e64ce62b745d1163e1
Jean-Christophe <jc.rondy@hd.energy>
2021-12-02 10:34:35 -0500

    first part of day two

--- src/premiereSemaine.py
+++ src/premiereSemaine.py
@@ -1,6 +1,6 @@
-def list_from_text(name_of_file):
+def int_list_from_text(name_of_file):
     output = []
-    with open("day1input.txt") as f:
+    with open(name_of_file) as f:
         for line in f:
             output.extend([int(x) for x in line.split()])
     return output
@@ -11,12 +11,34 @@ def sliding_comparer(list_like):
         [1 for previous, current in zip(list_like, list_like[1:]) if current > previous]
     )
 
+def tuple_list_from_text(name_of_file):
+    output = []
+    with open(name_of_file) as f:
+        for line in f:
+            inter = [x for x in line.split() ]
+            inter[1] = int(inter[1])
+            output.append(tuple(inter))
+    return output
 
-def main():
+def generate_direction(direction_tuple):
+    direction = direction_tuple[0]
+    strength = direction_tuple[1]
+
+    if direction=="forward":
+        output = (strength,0)
+    elif direction=="up":
+        output = (0,-strength)
+    elif direction == "down":
+        output = (0, strength)
+    else:
+        output = (0,0)
+    return output
+
+
+def firstDay():
     # puzzles of the first day: finding how many entries in a list are bigger than the previous one,
     # as well as finding how many group of three entries are bigger than the previous one.
-
-    firstDay = list_from_text("day1input.txt")
+    firstDay = int_list_from_text("day2input.txt")
     print(firstDay)
 
     count = sliding_comparer(firstDay)
@@ -28,6 +50,14 @@ def main():
     new_count = sliding_comparer(sliding_window)
     print(new_count)
 
+def main():
+    travel = [0,0]
+    test = tuple_list_from_text("src/day2input.txt")
+    for tup in test:
+        dir = generate_direction(tup)
+        travel[0] += dir[0]
+        travel[1] += dir[1]
 
+    print(travel)
 if __name__ == "__main__":
     main()
--- tests/test_first_week.py
+++ tests/test_first_week.py
@@ -1,18 +1,48 @@
 import unittest
 import pytest
-from src.premiereSemaine import sliding_comparer
+from src.premiereSemaine import sliding_comparer,tuple_list_from_text,generate_direction
 from parameterized import parameterized
+import os
 
+@pytest.fixture(autouse=True)
+def day_two_file():
+    output_list = [("a","1"),("b","2"),("c","3")]
+    with open("test_day_two.txt", "w") as fp:
+        fp.write('\n'.join("{} {}".format(x[0], x[1]) for x in output_list))
+    yield
+    os.remove("test_day_two.txt")
 
 @pytest.mark.parametrize(
     "test_input, expected",
     [
         ([1, 2, 3], 2),
         ([0, 0, 0], 0),
+        ([0, 1, 0], 1),
+        ([-1, 0, 1], 2),
     ],
 )
 def test_sliding_comparer_pytest(test_input, expected):
     assert sliding_comparer(test_input) == expected
+
+
+def test_tuple_list_from_text(day_two_file):
+    results = tuple_list_from_text("test_day_two.txt")
+    print("here")
+    assert (type(results[0]) == type((1,2))) &  (type(results[0][0])==type("string")) &  (type(results[0][1])==type(1))
+
+
+@pytest.mark.parametrize(
+    "test_input, expected",
+    [
+        (("forward",1),(1,0)),
+        (("down",1),(0,1)),
+        (("up",1),(0,-1)),
+    ]
+)
+def test_reading_directions(test_input, expected):
+    test_case = ("forward",1)
+
+    assert generate_direction(test_input)==expected
 
 
 # #using parameterized to test it
