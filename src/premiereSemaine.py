6d6fa943cf6762d6b491f19be37a36135326eae6
Jean-Christophe <jc.rondy@hd.energy>
2021-12-05 17:17:34 -0500

    day 3 part 2

--- src/premiereSemaine.py
+++ src/premiereSemaine.py
@@ -7,54 +7,71 @@ def int_list_from_text(name_of_file):
 
 
 def sliding_comparer(list_like):
-    return len( # Idée d'un collègue; plus rapide
-        [None for previous, current in zip(list_like, list_like[1:]) if current > previous]
+    return len(  # Idée d'un collègue; plus rapide
+        [
+            None
+            for previous, current in zip(list_like, list_like[1:])
+            if current > previous
+        ]
     )
+
 
 def tuple_list_from_text(name_of_file):
     output = []
     with open(name_of_file) as f:
         for line in f:
-            inter = [x for x in line.split() ]
+            inter = [x for x in line.split()]
             inter[1] = int(inter[1])
             output.append(tuple(inter))
     return output
+
 
 def generate_direction_part_one(direction_tuple):
     direction = direction_tuple[0]
     strength = direction_tuple[1]
 
-    if direction=="forward":
-        output = (strength,0)
-    elif direction=="up":
-        output = (0,-strength)
+    if direction == "forward":
+        output = (strength, 0)
+    elif direction == "up":
+        output = (0, -strength)
     elif direction == "down":
         output = (0, strength)
     else:
-        output = (0,0)
+        output = (0, 0)
     return output
 
-def generate_direction_part_two(direction_tuple,current_aim):
+
+def generate_direction_part_two(direction_tuple, current_aim):
     direction = direction_tuple[0]
     strength = direction_tuple[1]
 
-    if direction=="forward":
-        output = (strength,current_aim*strength,current_aim)
-    elif direction=="up":
-        output = (0,0,current_aim-strength)
+    if direction == "forward":
+        output = (strength, current_aim * strength, current_aim)
+    elif direction == "up":
+        output = (0, 0, current_aim - strength)
     elif direction == "down":
-        output = (0, 0,current_aim+strength)
+        output = (0, 0, current_aim + strength)
     else:
-        output = (0,0,current_aim)
+        output = (0, 0, current_aim)
     return output
 
+
 def accumulate_binary(array_of_array):
-    ratio = len(array_of_array)//2
-    return list(map(lambda x: 1 if x>ratio else 0 ,list(map(sum,zip(*array_of_array)))))
+    ratio = len(array_of_array) / 2
+    return list(
+        map(lambda x: 1 if x > ratio else 0, list(map(sum, zip(*array_of_array))))
+    )
+
+def accumulate_binary_oxygen(array_of_array):
+    ratio = len(array_of_array) / 2
+    return list(
+        map(lambda x: 1 if x >= ratio else 0, list(map(sum, zip(*array_of_array))))
+    )
 
 def gamma_to_epsilon(list_of_bits):
-    test = list(map( lambda x: 1 if x==0 else 0 , list_of_bits))
+    test = list(map(lambda x: 1 if x == 0 else 0, list_of_bits))
     return test
+
 
 def list_of_bit_to_int(list_of_bits):
     out = 0
@@ -62,15 +79,83 @@ def list_of_bit_to_int(list_of_bits):
         out = (out << 1) | bit
     return out
 
+
 def day_3_io(path):
     arr = []
     with open(path) as f:
         for line in f:
-            if line[-1] == '\n':
+            if line[-1] == "\n":
                 line = line[:-1]
             arr.append([int(i) for i in list(line)])
-    return(arr)
+    return arr
 
+
+def validate_card(i_card, i_number):
+    # check horizontal
+    for row in i_card:
+        if all(row):
+            return True
+    columns = zip(*i_card)
+    for column in columns:
+        if all(column):
+            return True
+
+    first_diagonal = []
+    second_diagonal = []
+    for index, row in enumerate(i_card):
+        first_diagonal.append(row[index])
+        second_diagonal.append(row[-(index + 1)])
+    if all(first_diagonal):
+        return True
+    if all(second_diagonal):
+        return True
+
+    return False
+
+
+def oxygen_rate(input_list):
+    list(map(sum, zip(*input_list)))
+    candidates = list(input_list)
+    max_n = len(input_list[0])
+
+    for id in range(max_n):
+        most_common_bit = accumulate_binary_oxygen(candidates)[id]
+        update = []
+        for candidate in candidates:
+            if most_common_bit == candidate[id]:
+                update.append(candidate)
+        if len(candidates)==0:
+            return 0
+        if len(candidates)==1:
+            break
+        candidates = update
+
+    if len(candidates) ==1:
+        return candidates[0]
+
+    return 0
+
+def carbon_rate(input_list):
+    list(map(sum, zip(*input_list)))
+    candidates = list(input_list)
+    max_n = len(input_list[0])
+
+    for id in range(max_n):
+        most_common_bit = gamma_to_epsilon(accumulate_binary_oxygen(candidates))[id]
+        update = []
+        for candidate in candidates:
+            if most_common_bit == candidate[id]:
+                update.append(candidate)
+        if len(candidates)==0:
+            return 0
+        if len(candidates)==1:
+            break
+        candidates = update
+
+    if len(candidates) ==1:
+        return candidates[0]
+
+    return 0
 def firstDay():
     # puzzles of the first day: finding how many entries in a list are bigger than the previous one,
     # as well as finding how many group of three entries are bigger than the previous one.
@@ -86,30 +171,37 @@ def firstDay():
     new_count = sliding_comparer(sliding_window)
     print(new_count)
 
+
 def secondDay():
-    travel = [0,0]
+    travel = [0, 0]
     input = tuple_list_from_text("src/day2input.txt")
     for tup in input:
         dir = generate_direction_part_one(tup)
         travel[0] += dir[0]
         travel[1] += dir[1]
 
-    travel = [0,0,0]
+    travel = [0, 0, 0]
     for tup in input:
-        dir = generate_direction_part_two(tup,travel[2])
+        dir = generate_direction_part_two(tup, travel[2])
         travel[0] += dir[0]
         travel[1] += dir[1]
         travel[2] = dir[2]
-    print(travel[0]*travel[1])
+    print(travel[0] * travel[1])
 
 
 def third_day():
-    gamma = accumulate_binary( day_3_io("src/day3input.txt") )
+    gamma = accumulate_binary(day_3_io("src/day3input.txt"))
     epsilon = gamma_to_epsilon(gamma)
-    print(list_of_bit_to_int(gamma)*list_of_bit_to_int(epsilon))
+    print(list_of_bit_to_int(gamma) * list_of_bit_to_int(epsilon))
 
+    arr = day_3_io("src/day3input.txt")
+    oxygen = list_of_bit_to_int(oxygen_rate(arr))
+    carbon = list_of_bit_to_int(carbon_rate(arr))
+    print(oxygen*carbon)
 def main():
     print("oh oh oh!")
     third_day()
+
+
 if __name__ == "__main__":
     main()
--- tests/test_first_week.py
+++ tests/test_first_week.py
@@ -8,7 +8,11 @@ from src.premiereSemaine import (
     accumulate_binary,
     gamma_to_epsilon,
     list_of_bit_to_int,
-    day_3_io
+    day_3_io,
+    validate_card,
+    oxygen_rate,
+    accumulate_binary_oxygen,
+    carbon_rate
 )
 from parameterized import parameterized
 import os
@@ -56,7 +60,7 @@ def test_tuple_list_from_text(day_two_file):
 )
 def test_reading_directions_part_one(test_input, expected):
 
-    assert generate_direction_part_two(test_input) == expected
+    assert generate_direction_part_one(test_input) == expected
 
 
 @pytest.mark.parametrize(
@@ -74,27 +78,24 @@ def test_reading_directions_part_two(test_input, expected):
     assert generate_direction_part_two(test_input[0], test_input[1]) == expected
 
 
-
 @pytest.mark.parametrize(
     "test_input, expected",
     [
-        ([[1,1,0],[1,0,1],[0,0,0]],([1,0,0])),
-        ([[1, 1, 0], [1, 0, 1], [0, 0, 1],[0,0,1]], ([0, 0, 1])),
-
-
+        ([[1, 1, 0], [1, 0, 1], [0, 0, 0]], ([1, 0, 0])),
+        ([[1, 1, 0], [1, 0, 1], [0, 0, 1], [0, 0, 1]], ([0, 0, 1])),
     ],
 )
 def test_accumulating_binary(test_input, expected):
 
     assert accumulate_binary(test_input) == expected
 
+
 @pytest.mark.parametrize(
     "test_input, expected",
     [
-        ([0,0,0,0],([1,1,1,1])),
+        ([0, 0, 0, 0], ([1, 1, 1, 1])),
         ([1, 1, 0, 0], ([0, 0, 1, 1])),
-        ([1,0,1,1,0],[0,1,0,0,1])
-
+        ([1, 0, 1, 1, 0], [0, 1, 0, 0, 1]),
     ],
 )
 def test_gamma_to_epsilon(test_input, expected):
@@ -104,23 +105,128 @@ def test_gamma_to_epsilon(test_input, expected):
 
 @pytest.mark.parametrize(
     "test_input, expected",
-    [
-        ([1,0],(2)),
-        ([1, 0,1,1, 0], (22)),
-        ([0,1,0,0,1],9)
-
-    ],
+    [([1, 0], (2)), ([1, 0, 1, 1, 0], (22)), ([0, 1, 0, 0, 1], 9)],
 )
-def test_list_of_bit_to_int(test_input,expected):
-    assert list_of_bit_to_int(test_input)==expected
+def test_list_of_bit_to_int(test_input, expected):
+    assert list_of_bit_to_int(test_input) == expected
+
 
 def test_day_three_part_one_integration():
-    #tests if the proper gamma rate is obtained
+    # tests if the proper gamma rate is obtained
     arr = day_3_io("../src/day3test.txt")
-
 
     total = accumulate_binary(arr)
     gamma = total
     epsilon = gamma_to_epsilon(total)
 
-    assert (list_of_bit_to_int(gamma)*list_of_bit_to_int(epsilon) == 198)
+    assert list_of_bit_to_int(gamma) * list_of_bit_to_int(epsilon) == 198
+
+
+@pytest.mark.parametrize(
+    "test_input, expected",
+    [
+        ([[1, 1, 0], [1, 1, 1], [0, 1, 1]], [1, 1, 1]),
+        ([[1, 1, 1], [1, 1, 0], [0, 1, 1]], [1, 1, 1]),
+        ([[0, 1, 1], [1, 1, 0], [1, 1, 1]], [1, 1, 1]),
+    ],
+)
+def test_oxygen_rate(test_input, expected):
+    assert oxygen_rate(test_input) == expected
+
+def test_day_three_part_two_integration():
+    # tests if the proper gamma rate is obtained
+    arr = day_3_io("../src/day3test.txt")
+
+
+    oxygen = list_of_bit_to_int(oxygen_rate(arr))
+    carbon = list_of_bit_to_int(carbon_rate(arr))
+
+
+    assert (oxygen == 23) & (carbon == 10 )
+
+
+
+
+@pytest.mark.parametrize(
+    "test_input, expected",
+    [
+        (
+            [
+                [0, 0, 0],
+                [0, 0, 0],
+                [0, 0, 0],
+            ],
+            False,
+        ),
+        (
+            [
+                [1, 0, 0],
+                [0, 0, 1],
+                [0, 1, 0],
+            ],
+            False,
+        ),
+        (
+            [
+                [1, 1, 1],
+                [0, 0, 0],
+                [0, 0, 0],
+            ],
+            True,
+        ),
+        (
+            [
+                [1, 0, 0],
+                [1, 0, 0],
+                [1, 0, 0],
+            ],
+            True,
+        ),
+        (
+            [
+                [1, 0, 0],
+                [0, 1, 0],
+                [0, 0, 1],
+            ],
+            True,
+        ),
+    ],
+)
+def test_day_four_validate_grids(test_input, expected):
+    assert validate_card(test_input) == expected
+
+
+@pytest.mark.parametrize(
+    "i_card,i_number, expected",
+    [
+        (
+            [
+                [10, 7, 9],
+                [99, 65, 11],
+                [78, 36, 24],
+            ],
+            65,
+            (1, 1),
+        ),
+        (
+            [
+                [10, 7, 9],
+                [99, 65, 11],
+                [78, 36, 24],
+            ],
+            11,
+            (2, 1),
+        ),
+        (
+            [
+                [10, 7, 9],
+                [99, 65, 11],
+                [78, 36, 24],
+            ],
+            299,
+            (-1, -1),
+        ),
+    ],
+)
+def test_day_four_check_new_number(i_card, i_number, expected):
+    assert validate_card(i_card, i_number) == expected
